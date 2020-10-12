584002120c61e2739bbceae1f0ebbb2b9b9ef885
user-lu <user.sevilu6@gmail.com>
2020-10-12 10:42:40 -0500

    added alerts to make sure buttons are functioning correctly

---
+++ index.js
@@ -0,0 +1,9 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+}
+
+function handleClick() {
+    alert("I got clicked!");
+}
