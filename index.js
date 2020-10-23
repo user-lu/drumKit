591d796819611b62bc63a1883517f85d17942a64
user-lu <user.sevilu6@gmail.com>
2020-10-23 10:07:23 -0500

    added sounds to the buttons

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+       let buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play(); 
+                break;
+                
+            case "a":        
+                let kickBass = new Audio("sounds/kick-bass.mp3");
+                kickBass.play(); 
+                break;
+            
+            case "s":        
+                let snare = new Audio("sounds/snare.mp3");
+                snare.play(); 
+                break;
+            
+            case "d":        
+                let tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play(); 
+                break;
+            
+            case "j":        
+                let tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play(); 
+                break;
+            
+            case "k":        
+                let tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play(); 
+                break;
+            
+            case "l":        
+                let tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play(); 
+                break;
+
+            default:
+        }
+        
+    });
+    
 }
 
-function handleClick() {
-    alert("I got clicked!");
-}
